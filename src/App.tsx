7a675379b7b9fb30ddeab0ecbb83fe524af53605
Zero <zeroto521@gmail.com>
2021-01-21 21:16:09 +0800

    :tada: finish 9.17

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App: React.FC = () => {
         data => setPatient(data)
       );
     }
-  }, []);
+  }, [patientMatch, patient]);
 
 
   return (
--- src/PatientListPage/index.tsx
+++ src/PatientListPage/index.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import axios from "axios";
-import { Container, Table, Button } from "semantic-ui-react";
+import axios from 'axios';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Container, Table } from 'semantic-ui-react';
 
-import { PatientFormValues } from "../AddPatientModal/AddPatientForm";
-import AddPatientModal from "../AddPatientModal";
-import { Patient } from "../types";
-import { apiBaseUrl } from "../constants";
-import HealthRatingBar from "../components/HealthRatingBar";
-import { useStateValue } from "../state";
+import AddPatientModal from '../AddPatientModal';
+import { PatientFormValues } from '../AddPatientModal/AddPatientForm';
+import HealthRatingBar from '../components/HealthRatingBar';
+import { apiBaseUrl } from '../constants';
+import { useStateValue } from '../state';
+import { Patient } from '../types';
 
 const PatientListPage: React.FC = () => {
   const [{ patients }, dispatch] = useStateValue();
@@ -53,7 +54,11 @@ const PatientListPage: React.FC = () => {
         <Table.Body>
           {Object.values(patients).map((patient: Patient) => (
             <Table.Row key={patient.id}>
-              <Table.Cell>{patient.name}</Table.Cell>
+              <Table.Cell>
+                <Link to={`/patients/${patient.id}`} >
+                  {patient.name}
+                </Link>
+              </Table.Cell>
               <Table.Cell>{patient.gender}</Table.Cell>
               <Table.Cell>{patient.occupation}</Table.Cell>
               <Table.Cell>
