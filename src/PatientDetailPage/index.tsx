d013b8b8ff493d4971e5a9083b4478046ecf627d
Zero <zeroto521@gmail.com>
2021-01-23 11:45:43 +0800

    :tada: finish 9.20

--- src/PatientDetailPage/index.tsx
+++ src/PatientDetailPage/index.tsx
@@ -1,13 +1,77 @@
 import React from 'react';
-import { Header, Icon, Table } from 'semantic-ui-react';
+import { Divider, Header, Icon, Table } from 'semantic-ui-react';
 
-import { Patient } from '../types';
+import { Entry, Patient } from '../types';
+
+const EntryDetail: React.FC<{ entry: Entry | null }> = ({ entry }) => {
+  if (!entry) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Header as="h4">{entry.id}</Header>
+      <div>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell></Table.HeaderCell>
+              <Table.HeaderCell>Attribute</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>description</Table.Cell>
+              <Table.Cell>{entry.description}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>date</Table.Cell>
+              <Table.Cell>{entry.date}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>specialist</Table.Cell>
+              <Table.Cell>{entry.specialist}</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </div>
+      <Divider />
+    </div>
+  );
+};
+
+
+const Entries: React.FC<{ entries: Entry[] | null }> = ({ entries }) => {
+  if (!entries) {
+    return null;
+  } else if (entries.length === 0) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Header as="h3">entries</Header>
+      <div>
+        {
+          entries.map(e =>
+            <EntryDetail key={e.id} entry={e} />
+          )
+        }
+      </div>
+    </div>
+
+  );
+};
 
 const PatientDetailPage: React.FC<{ patient: Patient | null | undefined }> = ({ patient }) => {
-  if (patient) {
-    const iconName = patient.gender === 'male' ? 'mars' : 'venus';
-    return (
-      <div className="App">
+  if (!patient) {
+    return null;
+  }
+
+  const iconName = patient.gender === 'male' ? 'mars' : 'venus';
+  return (
+    <div className="App">
+      <div>
         <Header as="h2">
           {patient.name} <Icon fitted name={iconName} />
         </Header>
@@ -34,10 +98,10 @@ const PatientDetailPage: React.FC<{ patient: Patient | null | undefined }> = ({ 
           </Table.Body>
         </Table>
       </div>
-    );
-  }
-
-  return null;
+      <Divider hidden />
+      <Entries entries={patient.entries} />
+    </div >
+  );
 };
 
 export default PatientDetailPage;
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,10 @@
-export interface Diagnosis {
+interface Diagnose {
   code: string;
   name: string;
   latin?: string;
 }
+
+export type Diagnosis = Diagnose;
 
 export enum Gender {
   Male = "male",
@@ -10,11 +12,59 @@ export enum Gender {
   Other = "other"
 }
 
+enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HealthCheckEntry
+  | OccupationalHealthcareEntry
+  | HospitalEntry;
+
 export interface Patient {
   id: string;
   name: string;
-  occupation: string;
+  dateOfBirth: string;
+  ssn: string;
   gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+  occupation: string;
+  entries: Entry[];
 }
